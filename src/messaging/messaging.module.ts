b76542d8c9a6d3f5afc76b3afd846c36e0a629b7
pooetitu <pooetitu@gmail.com>
2023-06-02 15:01:59 +0200

    [BM-185]: remove controller definition

--- src/messaging/messaging.module.ts
+++ src/messaging/messaging.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessagingService } from './messaging.service';
-import { MessagingController } from './messaging.controller';
 import { MessageEntity } from './message.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageEntity])],
   providers: [MessagingService],
-  controllers: [MessagingController],
+  controllers: [],
   exports: [MessagingService],
 })
 export class MessagingModule {}
