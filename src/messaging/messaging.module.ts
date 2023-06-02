7a3abd035c45a377085aad56614c25562d4b7579
pooetitu <pooetitu@gmail.com>
2023-06-02 19:29:20 +0200

    .

--- src/messaging/messaging.module.ts
+++ src/messaging/messaging.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessagingService } from './messaging.service';
 import { MessageEntity } from './message.entity';
+import { MessagingGateway } from './messaging.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageEntity])],
-  providers: [MessagingService],
+  providers: [MessagingService, MessagingGateway],
   exports: [MessagingService],
 })
 export class MessagingModule {}
