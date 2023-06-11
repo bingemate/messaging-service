894622a61efcf8b8f873538d7fe5ed44388cb633
pooetitu <pooetitu@gmail.com>
2023-06-11 20:43:40 +0200

    Add exception protection on gateway

--- src/messaging/messaging.gateway.ts
+++ src/messaging/messaging.gateway.ts
@@ -23,23 +23,31 @@ export class MessagingGateway
     @ConnectedSocket() client: Socket,
     @MessageBody() createMessage: CreateMessageDto,
   ): Promise<void> {
-    const message = await this.messagingService.createMessage({
-      senderId: client.handshake.headers['user-id'] as string,
-      receiverId: createMessage.receiverId,
-      text: createMessage.text,
-    });
-    client.emit('newMessage', message);
-    if (this.clients.has(createMessage.receiverId)) {
-      this.clients.get(createMessage.receiverId).emit('newMessage', message);
+    try {
+      const message = await this.messagingService.createMessage({
+        senderId: client.handshake.headers['user-id'] as string,
+        receiverId: createMessage.receiverId,
+        text: createMessage.text,
+      });
+      client.emit('newMessage', message);
+      if (this.clients.has(createMessage.receiverId)) {
+        this.clients.get(createMessage.receiverId).emit('newMessage', message);
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
   @SubscribeMessage('getMessages')
   async getMessages(@ConnectedSocket() client: Socket): Promise<void> {
-    const messages = await this.messagingService.getMessages(
-      client.handshake.headers['user-id'] as string,
-    );
-    client.emit('messages', messages);
+    try {
+      const messages = await this.messagingService.getMessages(
+        client.handshake.headers['user-id'] as string,
+      );
+      client.emit('messages', messages);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   @SubscribeMessage('deleteMessage')
@@ -47,15 +55,19 @@ export class MessagingGateway
     @ConnectedSocket() client: Socket,
     @MessageBody() deleteMessage: DeleteMessageDto,
   ): Promise<void> {
-    const msg = await this.messagingService.getMessageById(
-      deleteMessage.messageId,
-    );
-    await this.messagingService.deleteMessage(
-      deleteMessage.messageId,
-      client.handshake.headers['user-id'] as string,
-    );
-    if (this.clients.has(msg.receiverId)) {
-      this.clients.get(msg.receiverId).emit('deletedMessage', deleteMessage);
+    try {
+      const msg = await this.messagingService.getMessageById(
+        deleteMessage.messageId,
+      );
+      await this.messagingService.deleteMessage(
+        deleteMessage.messageId,
+        client.handshake.headers['user-id'] as string,
+      );
+      if (this.clients.has(msg.receiverId)) {
+        this.clients.get(msg.receiverId).emit('deletedMessage', deleteMessage);
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
