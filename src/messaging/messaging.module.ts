414665d89203148090281d5c10e7cca75254f729
pooetitu <pooetitu@gmail.com>
2023-06-24 15:56:41 +0200

    Add controller to module

--- src/messaging/messaging.module.ts
+++ src/messaging/messaging.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { MessagingService } from './messaging.service';
 import { MessageEntity } from './message.entity';
 import { MessagingGateway } from './messaging.gateway';
+import { MessagingController } from './messaging.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageEntity])],
   providers: [MessagingService, MessagingGateway],
   exports: [MessagingService],
+  controllers: [MessagingController],
 })
 export class MessagingModule {}
